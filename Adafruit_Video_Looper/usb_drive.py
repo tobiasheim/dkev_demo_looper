199e94ae17250ab56b81dfdb1c9323c3205a733b
Tobi <tobias.heim@gmail.com>
2017-11-02 21:56:59 +0100

    first changes

--- Adafruit_Video_Looper/usb_drive.py
+++ Adafruit_Video_Looper/usb_drive.py
@@ -38,7 +38,7 @@ class USBDriveReader(object):
 
     def idle_message(self):
         """Return a message to display when idle and no files are found."""
-        return 'Insert USB drive with compatible movies.'
+        return 'Nothing to see here. Insert USB drive with compatible movies.'
 
 
 def create_file_reader(config):
